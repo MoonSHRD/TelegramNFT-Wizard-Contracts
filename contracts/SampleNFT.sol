7d6b9cf58b1e037ded1dd8e7537b155c39ca05f3
Bekket <jackatwork@yandex.ru>
2022-11-10 17:01:38 +0400

    sample nft

---
+++ contracts/SampleNFT.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+// collection, singleton NFT(?)
+contract SampleNFT is ERC721URIStorage{
+
+    using Counters for Counters.Counter;
+    Counters.Counter token_ids_count;
+
+    constructor(string memory name_, string memory smbl_) ERC721(name_, smbl_) ERC721URIStorage() {
+
+    }
+
+
+    /**
+     *  @dev create nft (single record in this contract) with provided file_id and returns token_id
+     *  @param file_id unique file_id string obtained from telegram(?)
+     */
+    function CreateItem(string memory file_id) public returns (uint256 token_id) {
+        token_ids_count.increment();
+        token_id = token_ids_count.current();
+        _safeMint(msg.sender,token_id);
+        _setTokenURI(token_id,file_id);
+
+    }
+
+
+}
