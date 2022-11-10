69d37d61bb565132cb492740d5ef368bd5aa4a0a
Bekket <jackatwork@yandex.ru>
2022-11-10 17:32:25 +0400

    sample nft, singleton nft

--- contracts/SingletonNFT.sol
+++ contracts/SingletonNFT.sol
@@ -8,32 +8,29 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 
 /**
- *  Sample NFT which use as a dummy from factory
+ *  Singleton NFT
  *  Each file is a record inside this single contract
- *  Constructor accept file_ids string[] array and create tokens from it
  */
-contract SampleNFT is ERC721URIStorage{
+contract SingletonNFT is ERC721URIStorage{
 
     using Counters for Counters.Counter;
     Counters.Counter token_ids_count;
+
     event ItemCreated (string file_id, uint256 token_id);
 
-    constructor(string memory name_, string memory smbl_,string[] memory file_ids, address minter_) ERC721(name_, smbl_) ERC721URIStorage() {
-        for (uint i=0; i<file_ids.length;i++) {
-            CreateItem(file_ids[i],minter_);
-        }
+    constructor(string memory name_, string memory smbl_) ERC721(name_, smbl_) ERC721URIStorage() {
+
     }
 
 
     /**
      *  @dev create nft (single record in this contract) with provided file_id and returns token_id
      *  @param file_id unique file_id string obtained from telegram(?)
-     *  @param minter_ address of token minter, I've changed it from msg.sender to this,cause constructor may be called from factory contract
      */
-    function CreateItem(string memory file_id, address minter_) public returns (uint256 token_id) {
+    function CreateItem(string memory file_id) public returns (uint256 token_id) {
         token_ids_count.increment();
         token_id = token_ids_count.current();
-        _safeMint(minter_,token_id);
+        _safeMint(msg.sender,token_id);
         _setTokenURI(token_id,file_id);
         emit ItemCreated(file_id,token_id);
     }
